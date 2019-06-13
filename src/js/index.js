61f4d902fbed310e054acecbfd47e61d40c03f32
VAO61 <vao.frontend@gmail.com>
2019-06-13 18:54:23 +0300

    Added draft

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,6 @@ import '../sass/style.scss';
 if (process.env.NODE_ENV !== 'production') {
   require('file-loader!../html/index.html');
 }
+
+// TODO: Из-за подключенного .js перестает релодиться html
+import './api/bacon.js';
