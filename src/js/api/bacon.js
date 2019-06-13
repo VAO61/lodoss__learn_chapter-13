4c6b8d0e812f8ec6b99d02563231952b68d2e0bf
VAO61 <vao.frontend@gmail.com>
2019-06-13 19:11:57 +0300

    UPD: draft

--- src/js/api/bacon.js
+++ src/js/api/bacon.js
@@ -5,36 +5,39 @@
 // button.addEventListener('click', () => {});
 // });
 
-// var getJSON = function(url, callback) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', url, true);
-//   xhr.responseType = 'json';
-//   xhr.onload = function() {
-//     var status = xhr.status;
-//     if (status === 200) {
-//       callback(null, xhr.response);
-//     } else {
-//       callback(status, xhr.response);
-//     }
-//   };
-//   xhr.send();
-// };
+var getJSON = function(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      console.log('xhr.response', xhr.response);
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
 
 const button = document.getElementById('baconButton');
 
 button.addEventListener('click', () => {
   let baconIpsumOutput = document.getElementById('someTestBlock');
-  baconIpsumOutput.append();
+  // baconIpsumOutput.append();
 
-  // getJSON(
-  //   'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html',
-  //   function(baconGoodness) {
-  //     if (baconGoodness && baconGoodness.length > 0) {
-  //       baconIpsumOutput.html('');
-  //       for (var i = 0; i < baconGoodness.length; i++)
-  //         baconIpsumOutput.append('<p>' + baconGoodness[i] + '</p>');
-  //       baconIpsumOutput.show();
-  //     }
-  //   }
-  // );
+  const res = getJSON(
+    'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=json',
+    function(errCode, baconGoodness) {
+      console.log('baconGoodness', baconGoodness);
+      if (baconGoodness && baconGoodness.length > 0) {
+        // baconIpsumOutput.html('');
+        for (var i = 0; i < baconGoodness.length; i++)
+          baconIpsumOutput.append('<p>' + baconGoodness[i] + '</p>');
+        // baconIpsumOutput.show();
+      }
+    }
+  );
+  console.log('res', res);
 });
