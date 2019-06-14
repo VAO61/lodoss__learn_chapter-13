270a982fb1f0b47ca72521aca6ac434c043adec1
VAO61 <vao.frontend@gmail.com>
2019-06-14 15:13:15 +0300

    BaconJsonApi v0.0.1 --pre-alpha

--- src/js/api/bacon.js
+++ src/js/api/bacon.js
@@ -1,43 +1,35 @@
-// document.addEventListener('DOMContentLoaded', function(event) {
-// const button = document.getElementById('baconButton');
-// let baconIpsumOutput = document.getElementById('someTestBlock');
-
-// button.addEventListener('click', () => {});
-// });
-
-var getJSON = function(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.onload = function() {
-    var status = xhr.status;
-    if (status === 200) {
-      console.log('xhr.response', xhr.response);
-      callback(null, xhr.response);
-    } else {
-      callback(status, xhr.response);
-    }
-  };
-  xhr.send();
-};
-
-const button = document.getElementById('baconButton');
-
-button.addEventListener('click', () => {
-  let baconIpsumOutput = document.getElementById('someTestBlock');
-  // baconIpsumOutput.append();
-
-  const res = getJSON(
-    'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=json',
-    function(errCode, baconGoodness) {
-      console.log('baconGoodness', baconGoodness);
-      if (baconGoodness && baconGoodness.length > 0) {
-        // baconIpsumOutput.html('');
-        for (var i = 0; i < baconGoodness.length; i++)
-          baconIpsumOutput.append('<p>' + baconGoodness[i] + '</p>');
-        // baconIpsumOutput.show();
+document.addEventListener('DOMContentLoaded', function(event) {
+  var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        console.log('xhr.response', xhr.response);
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
       }
-    }
-  );
-  console.log('res', res);
+    };
+    xhr.send();
+  };
+
+  const button = document.getElementById('baconButton');
+
+  button.addEventListener('click', () => {
+    let baconIpsumOutput = document.getElementById('someTestBlock');
+
+    getJSON(
+      'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=json',
+      function(errCode, baconGoodness) {
+        console.log('baconGoodness', baconGoodness);
+        if (baconGoodness && baconGoodness.length > 0) {
+          for (var i = 0; i < baconGoodness.length; i++)
+            // baconIpsumOutput.append('<p>' + baconGoodness[i] + '</p>');
+            baconIpsumOutput.innerHTML = '<p>' + baconGoodness[i] + '</p>';
+        }
+      }
+    );
+  });
 });
