09aa477de0f6c4f1bfeb60aadbbb33c4e77a362e
Unknown <iwork.mail@mail.ru>
2019-06-15 23:26:25 +0300

    UPD: BaconJsonApi v0.0.2 alpha

--- src/js/api/bacon.js
+++ src/js/api/bacon.js
@@ -1,39 +1,65 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        console.log('xhr.response', xhr.response);
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr.send();
+const stringParams = obj =>
+  Object.keys(obj)
+    .map(key => `${key}=${obj[key]}`)
+    .join('&');
+
+// document.addEventListener('DOMContentLoaded', function(event) {
+var getJSON = function(url, params, callbackSuccess, callbackError) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', `${url}?${stringParams(params)}`, true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      // console.log('xhr.response', xhr.response);
+      callbackSuccess(xhr.response);
+    } else {
+      callbackError(status);
+    }
   };
+  xhr.send();
+};
 
-  const button = document.getElementById('baconButton');
+const button = document.getElementById('baconButton');
 
-  button.addEventListener('click', () => {
-    let baconIpsumOutput = document.getElementById('someTestBlock');
-    // TODO: Находит, но не записывает (innerHTML)
-    // let baconIpsumOutput = document.getElementsByClassName('block-lorem__item');
+const getData = () => {
+  // let baconIpsumOutput = document.getElementById('someTestBlock');
+  // TODO: Находит, но не записывает (innerHTML)
+  let baconIpsumOutput = document.querySelector('.block-lorem');
 
-    console.log(baconIpsumOutput);
+  // console.log(baconIpsumOutput);
 
-    getJSON(
-      'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=json',
-      function(errCode, baconGoodness) {
-        console.log('baconGoodness', baconGoodness);
-        if (baconGoodness && baconGoodness.length > 0) {
-          for (var i = 0; i < baconGoodness.length; i++)
-            // baconIpsumOutput.append('<p>' + baconGoodness[i] + '</p>');
-            baconIpsumOutput.innerHTML = '<p>' + baconGoodness[i] + '</p>';
+  getJSON(
+    'https://baconipsum.com/api/',
+    {
+      type: 'all-meat',
+      paras: 3,
+      'start-with-lorem': 1,
+      format: 'json'
+    },
+    function(baconGoodness) {
+      if (baconGoodness && baconGoodness.length > 0) {
+        baconIpsumOutput.innerHTML = '';
+        for (var i = 0; i < baconGoodness.length; i++) {
+          const div = document.createElement('div');
+          div.className = 'block-lorem__item';
+          const p = document.createElement('p');
+          p.innerHTML = baconGoodness[i];
+          div.appendChild(p);
+          // baconIpsumOutput.append('<p>' + baconGoodness[i] + '</p>');
+          baconIpsumOutput.appendChild(div);
         }
       }
-    );
-  });
-});
+    },
+    function(codeError) {
+      alert(`Error ${codeError}`);
+    }
+  );
+};
+
+button.addEventListener('click', getData);
+
+document.addEventListener('DOMContentLoaded', getData);
+// });
+
+// API не соответствует макету/формулировке задания!
