ef6c9ff72e4e604a31dabedafbcff4d3bf08d2bd
VAO61 <vao.frontend@gmail.com>
2019-06-25 10:32:41 +0300

    Third draft / --pre-alpha

--- src/js/api/getJSON.js
+++ src/js/api/getJSON.js
@@ -3,45 +3,31 @@ const stringParams = obj =>
     .map(key => `${key}=${obj[key]}`)
     .join('&');
 
-var getJSON = function(url, params, callbackSuccess, callbackError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', `${url}?${stringParams(params)}`, true);
-  xhr.responseType = 'json';
-  xhr.onload = function() {
-    var status = xhr.status;
-    if (status === 200) {
-      callbackSuccess(xhr.response);
-    } else {
-      callbackError(status);
-    }
-  };
-  xhr.send();
+var getJSON = function(url, params) {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `${url}?${stringParams(params)}`, true);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        // if (status === 200) {
+        resolve(xhr.response);
+        console.log(xhr.response);
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText + 'What`s a problem mother f..ather?'
+        });
+      }
+    };
+    xhr.onerror = () => {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
 };
-
-// var getJSON = function(url, params) {
-//   return new Promise((resolve, reject) => {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', `${url}?${stringParams(params)}`, true);
-//     xhr.responseType = 'json';
-//     xhr.onload = () => {
-//       if (status >= 200 && this.status < 300) {
-//         // if (status === 200) {
-//         resolve(xhr.response);
-//       } else {
-//         reject({
-//           status: status,
-//           statusText: xhr.statusText
-//         });
-//       }
-//     };
-//     xhr.onerror = () => {
-//       reject({
-//         status: status,
-//         statusText: xhr.statusText
-//       });
-//     };
-//     xhr.send();
-//   });
-// };
 
 module.exports = getJSON;
--- src/js/api/main.js
+++ src/js/api/main.js
@@ -10,48 +10,27 @@ const getData = () => {
     const images = imageList.map(
       item => `https://picsum.photos/id/${item.id}/140/140`
     );
-
-    getJSON(
-      'https://baconipsum.com/api/',
-      {
-        type: 'meat-and-filler',
-        paras: countItems,
-        'start-with-lorem': 5,
-        format: 'json'
-      },
-      function(strings) {
-        rendering(output, strings, images);
-      },
-      function(codeError) {
-        console.error(`Error ${codeError}`);
-      }
-    );
-  });
-
-  //   getJSON('https://picsum.photos/v2/list', { limit: countItems }, imageList => {
-  //     const images = imageList
-  //       .map(item => `https://picsum.photos/id/${item.id}/140/140`)
-  //       .then(function(datums) {
-  //         console.log(datums);
-  //         return images;
-  //       })
-  //       .catch(function(err) {
-  //         console.log(err.statusText);
-  //       });
-  //   });
-
-  //   getJSON('https://baconipsum.com/api/', {
-  //     type: 'meat-and-filler',
-  //     paras: countItems,
-  //     'start-with-lorem': 5,
-  //     format: 'json'
-  //   })
-  //     .then(function() {
-  //       rendering(output, strings, images);
-  //     })
-  //     .catch(function(err) {
-  //       console.log(err.statusText);
-  //     });
+  })
+    .then(function(dataImages) {
+      console.log(dataImages);
+      return images;
+    })
+    .catch(function(err) {
+      console.log(err.statusText);
+    });
+  getJSON('https://baconipsum.com/api/', {
+    type: 'meat-and-filler',
+    paras: countItems,
+    'start-with-lorem': 5,
+    format: 'json'
+  })
+    .then(function(rendering) {
+      rendering(output, strings, images);
+      console.log(rendering);
+    })
+    .catch(function(err) {
+      console.log(err.statusText);
+    });
 };
 
 getData();
