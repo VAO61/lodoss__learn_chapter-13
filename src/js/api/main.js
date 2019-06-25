68de4568705e5637f0831046066ead18077b8964
VAO61 <vao.frontend@gmail.com>
2019-06-25 13:06:36 +0300

    Added new task

--- src/js/api/main.js
+++ src/js/api/main.js
@@ -4,33 +4,43 @@ const rendering = require('./rendering.js');
 const countItems = 6;
 
 const getData = () => {
-  let output = document.querySelector('.random-content');
+  // let output = document.querySelector('.random-content');
+  // let images = []
+  // let strings = []
+  //   return getJSON('https://picsum.photos/v2/list', { limit: countItems })
+  //   .then(res => {
+  //     // res, map
+  //     images = imageList.map(
+  //       item => `https://picsum.photos/id/${item.id}/140/140`
+  //     );
+  //     return getJSON('https://baconipsum.com/api/');
+  //   }).then(string => {
+  //       strings = ...;
+
+  //   })
+  //   .catch();
 
   getJSON('https://picsum.photos/v2/list', { limit: countItems }, imageList => {
     const images = imageList.map(
       item => `https://picsum.photos/id/${item.id}/140/140`
     );
-  })
-    .then(function(dataImages) {
-      console.log(dataImages);
-      return images;
-    })
-    .catch(function(err) {
-      console.log(err.statusText);
-    });
-  getJSON('https://baconipsum.com/api/', {
-    type: 'meat-and-filler',
-    paras: countItems,
-    'start-with-lorem': 5,
-    format: 'json'
-  })
-    .then(function(rendering) {
-      rendering(output, strings, images);
-      console.log(rendering);
-    })
-    .catch(function(err) {
-      console.log(err.statusText);
-    });
+
+    getJSON(
+      'https://baconipsum.com/api/',
+      {
+        type: 'meat-and-filler',
+        paras: countItems,
+        'start-with-lorem': 5,
+        format: 'json'
+      },
+      function(strings) {
+        rendering(output, strings, images);
+      },
+      function(codeError) {
+        console.error(`Error ${codeError}`);
+      }
+    );
+  });
 };
 
 getData();
