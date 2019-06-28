51139ee83ffde922b29d06a362ba5eaf582eca01
Unknown <iwork.mail@mail.ru>
2019-06-29 01:54:00 +0300

    Promise 0.1.1

--- src/js/api/main.js
+++ src/js/api/main.js
@@ -2,39 +2,38 @@ const getJSON = require('./getJSON.js');
 const rendering = require('./rendering.js');
 const output = document.querySelector('.random-content');
 
-// getData(Number) : Promise<Array[{src: String, text: String}]>
 const getData = countItems => {
-  // let images;
+  const reqestImages = getJSON('https://picsum.photos/v2/list', {
+    limit: countItems
+  });
 
-  getJSON('https://picsum.photos/v2/list', { limit: countItems })
-    .then(imageList => {
-      images = imageList.map(
+  const reqestStrings = getJSON('https://baconipsum.com/api/', {
+    type: 'meat-and-filler',
+    paras: countItems,
+    'start-with-lorem': 5,
+    format: 'json'
+  });
+
+  // Promise.race - ожидает первый, который выполнится
+  Promise.all([reqestImages, reqestStrings])
+    .then(responses => {
+      const imageList = responses[0];
+      const strings = responses[1];
+
+      const images = imageList.map(
         item => `https://picsum.photos/id/${item.id}/140/140`
       );
 
-      return getJSON('https://baconipsum.com/api/', {
-        type: 'meat-and-filler',
-        paras: countItems,
-        'start-with-lorem': 5,
-        format: 'json'
-      });
-    })
-    .then(strings => {
-      data = [];
-      strings.map((item, index) => {
+      const data = strings.map((item, index) => {
         let obj = new Object();
         obj.desc = item;
         obj.image = images[index];
-        data.push(obj);
+        return obj;
       });
-      return data;
-    })
-    .then(data => {
+
       rendering(output, data);
     })
-    .catch(err => {
-      console.log(err.statusText);
-    });
+    .catch(err => console.log(err));
 };
 
 getData(6);
