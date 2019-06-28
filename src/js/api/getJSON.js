1f8e7a35433bdabaa1ec7b0c74f84bf2f2aa6a29
Unknown <iwork.mail@mail.ru>
2019-06-29 01:12:36 +0300

    async v0.1.0

--- src/js/api/getJSON.js
+++ src/js/api/getJSON.js
@@ -3,27 +3,23 @@ const stringParams = obj =>
     .map(key => `${key}=${obj[key]}`)
     .join('&');
 
-var getJSON = async (url, params) => {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', `${url}?${stringParams(params)}`, true);
-  xhr.responseType = 'json';
-  xhr.onload = () => {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      return xhr.response;
-    } else {
-      throw new Error({
-        status: xhr.status,
-        statusText: xhr.statusText
-      });
-    }
-  };
-  xhr.onerror = () => {
-    throw new Error({
-      status: xhr.status,
-      statusText: xhr.statusText
-    });
-  };
-  xhr.send();
+var getJSON = function(url, params) {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `${url}?${stringParams(params)}`, true);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr);
+      }
+    };
+    xhr.onerror = err => {
+      reject(err);
+    };
+    xhr.send();
+  });
 };
 
 export default getJSON;
--- src/js/api/main.js
+++ src/js/api/main.js
@@ -4,36 +4,34 @@ import rendering from './rendering.js';
 const output = document.querySelector('.random-content');
 
 // getData(Number) : Promise<Array[{src: String, text: String}]>
-const getData = countItems => {
-  getJSON('https://picsum.photos/v2/list', { limit: countItems })
-    .then(imageList => {
-      images = imageList.map(
-        item => `https://picsum.photos/id/${item.id}/140/140`
-      );
-
-      return getJSON('https://baconipsum.com/api/', {
-        type: 'meat-and-filler',
-        paras: countItems,
-        'start-with-lorem': 5,
-        format: 'json'
-      });
-    })
-    .then(strings => {
-      data = [];
-      strings.map((item, index) => {
-        let obj = new Object();
-        obj.desc = item;
-        obj.image = images[index];
-        data.push(obj);
-      });
-      return data;
-    })
-    .then(data => {
-      rendering(output, data);
-    })
-    .catch(err => {
-      console.log(err.statusText);
+const getData = async countItems => {
+  try {
+    const imageList = await getJSON('https://picsum.photos/v2/list', {
+      limit: countItems
     });
+
+    const images = imageList.map(
+      item => `https://picsum.photos/id/${item.id}/140/140`
+    );
+
+    const strings = await getJSON('https://baconipsum.com/api/', {
+      type: 'meat-and-filler',
+      paras: countItems,
+      'start-with-lorem': 5,
+      format: 'json'
+    });
+
+    const data = strings.map((item, index) => {
+      let obj = new Object();
+      obj.desc = item;
+      obj.image = images[index];
+      return obj;
+    });
+
+    rendering(output, data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-getData(6);
+getData(100);
