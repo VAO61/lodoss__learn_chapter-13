23e281e5409b91ed2da7b26efb88f6c59a6b73ca
VAO61 <vao.frontend@gmail.com>
2019-07-02 14:28:47 +0300

    Promise 0.2.0 (_mentor)

--- src/js/api/main.js
+++ src/js/api/main.js
@@ -16,19 +16,16 @@ const getData = countItems => {
 
   // Promise.race - ожидает первый, который выполнится
   Promise.all([reqestImages, reqestStrings])
-    .then(responses => {
-      const imageList = responses[0];
-      const strings = responses[1];
-
+    .then(([imageList, strings]) => {
       const images = imageList.map(
         item => `https://picsum.photos/id/${item.id}/140/140`
       );
 
       const data = strings.map((item, index) => {
-        let obj = new Object();
-        obj.desc = item;
-        obj.image = images[index];
-        return obj;
+        return {
+          desc: item,
+          image: images[index]
+        };
       });
 
       rendering(output, data);
